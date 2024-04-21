2b72a3bef005253d11cdbb6cbaf29e8009b47150
Martins Akeredolu <olamideakeredolu@gmail.com>
2024-04-21 16:00:42 +0100

    fix: fixed issue

--- src/utils/api_calls/product_api_calls/index.ts
+++ src/utils/api_calls/product_api_calls/index.ts
@@ -49,6 +49,20 @@ export const get_product_by_id = async (data: FilterParams) => {
   return response?.data;
 };
 
+export const get_product_by_slug = async (data: FilterParams) => {
+  const { product_slug } = data;
+
+  const res = await fetch(
+    `${BASE_URL}/products/product/fetch/${product_slug}`,
+    {
+      method: 'GET'
+    }
+  );
+
+  const response = await res.json();
+  return response?.data;
+};
+
 export const create_new_product = async (data: any) => {
   const res = await fetch(`${BASE_URL}/products/store`, {
     method: 'POST',
